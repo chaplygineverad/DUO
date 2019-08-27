fa8680803949499bd3dbce9fd60bbdb81171311d
chaplygineverad <s.chaplygin@everad.com>
2019-08-27 16:36:51 +0300

    added sections

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,3 @@
-//=include ../components/1-header/header.js
+//=include ../components/1-header/1-header.js
 //=include ../components/6-use-feed-delivery/6-use-feed-delivery.js
 //=include ../components/footer/footer.js
