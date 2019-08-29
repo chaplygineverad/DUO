dff044437d29ec0d9c1d8b1bcf52cb2e2cd2ddef
Kirill <nstkuleshov1@gmail.com>
2019-08-29 18:58:09 +0300

    js in radical

--- app/components/3-radical/3-radical.js
+++ app/components/3-radical/3-radical.js
@@ -2,8 +2,43 @@
     let linksPopup = document.querySelectorAll('.ever-popup-btn');
 
     for (let i = 0; i < linksPopup.length; i++) {
-        linksPopup[i].addEventListener('click', function(event) {
+        linksPopup[i].addEventListener('click', function (event) {
             event.preventDefault();
         })
     }
+}
+
+{
+    let popups = document.querySelectorAll('.danger__var');
+
+    for (let i = 0; i < popups.length; i++) {
+        popups[i].addEventListener('mouseover', function (event) {
+            let content = popups[i].querySelector('.danger__show');
+            content.style.cssText = 'display: block;';
+        });
+
+        popups[i].addEventListener('mouseout', function (event) {
+            let content = popups[i].querySelector('.danger__show');
+            content.style.cssText = 'display: none;';
+        });
+    }
+}
+
+{
+    let arrows = document.querySelectorAll('.danger__arrow');
+
+    let i = 0;
+    function glowing(array){
+        for (let i = 0; i < array.length; i++) {
+            array[i].style.opacity = 0;
+        }
+        array[i].style.opacity = 1;
+        i++;
+        if (i == arrows.length) {
+            i = 0;
+        }
+        setTimeout(glowing(array), 1000);
+    }
+
+    glowing(arrows);
 }
