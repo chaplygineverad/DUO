9fe261b72d6872c467e4224df2f690b0f1cfa989
Vladislad T <v.trundenko@everad.com>
2019-09-02 10:25:16 +0300

    vlad

--- app/components/6-use-feed-delivery/feed.js
+++ app/components/6-use-feed-delivery/feed.js
@@ -1,12 +1,12 @@
-// $(".reviews-content").on("afterChange", function(event, slick, currentSlide) {
-//   $(this)
-//     .find(".slick-slide.content-plus")
-//     .not(".slick-active")
-//     .removeClass("content-plus");
-// });
-// $(".reviews-content-box__cbtn").on("click", function() {
-//   this.parentElement.parentElement.classList.toggle("content-plus");
-// });
+$(".feed__content").on("afterChange", function(event, slick, currentSlide) {
+  $(this)
+    .find(".slick-slide.content-plus")
+    .not(".slick-active")
+    .removeClass("content-plus");
+});
+$(".feed__btn").on("click", function() {
+  this.parentElement.classList.toggle("content-plus");
+});
 $(".feed__content").slick({
   infinite: true,
   slidesToShow: 3,
@@ -34,7 +34,8 @@ $(".feed__content").slick({
       breakpoint: 640,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+				slidesToScroll: 1,
+				adaptiveHeight: true
       }
     }
   ]
